fdbebcb2374c2a34a3efe8d20feb0039ec386a12
= <iceoceaninn@gmail.com>
2021-01-12 13:37:28 +0800

    1.0.1

--- install-dat-release.sh
+++ install-dat-release.sh
@@ -2,8 +2,8 @@
 
 # This Bash script to install the latest release of geoip.dat and geosite.dat:
 
-# https://github.com/v2fly/geoip
-# https://github.com/v2fly/domain-list-community
+# https://github.com/oceantide/geoip
+# https://github.com/oceantide/domain-list-community
 
 # Depends on cURL, please solve it yourself
 
@@ -15,8 +15,8 @@
 
 # You can modify it to /usr/local/lib/v2ray
 V2RAY="/usr/local/share/v2ray"
-DOWNLOAD_LINK_GEOIP="https://github.com/v2fly/geoip/releases/latest/download/geoip.dat"
-DOWNLOAD_LINK_GEOSITE="https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat"
+DOWNLOAD_LINK_GEOIP="https://github.com/oceantide/geoip/releases/latest/download/geoip.dat"
+DOWNLOAD_LINK_GEOSITE="https://github.com/oceantide/domain-list-community/releases/latest/download/dlc.dat"
 file_ip='geoip.dat'
 file_dlc='dlc.dat'
 file_site='geosite.dat'
